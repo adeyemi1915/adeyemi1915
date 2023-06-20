d70900fca430cba31d5c8ccb49d7c4f3c8104e74
adeyemi1915 <135009534+adeyemi1915@users.noreply.github.com>
2023-06-20 11:51:55 +0300

    complete code challenge2

---
+++ challenge2.js
@@ -0,0 +1,20 @@
+function calculateModecafe (speed) {
+    const speedlimit=80;
+    const KmPerModecafe=5;
+    const maxModecafe=20;
+
+    if (speed<speedLimit){
+        console. log ("ok");
+        return;
+    }
+
+} 
+    const Modecafe = Math. floor ( (speed-speedLimit) /KmPerModecafe);{
+
+    if(modecafe<maxModecafe){
+            console.log("license suspended");
+    }else{
+            console.log(modecafe);
+    }
+
+}
